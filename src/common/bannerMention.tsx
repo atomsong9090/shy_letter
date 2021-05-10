3b2d3d4b1203cf7f12b470028f1d1ac92fc02947
atomsong92 <yesparrot@gmail.com>
2021-05-11 01:58:04 +0900

    [client] ADD 배너 추가 및 common 배너 멘션 추가

---
+++ src/common/bannerMention.tsx
@@ -0,0 +1,14 @@
+export default {
+  korean: {
+    lover: "당신에게 제 진심을 전하고 싶어요",
+    parent: "당신이 있어서 저도 있어요",
+    child: "당신이 있어서 저는 늘 행복해요",
+    friend: "당신과 친구여서 다행이에요",
+  },
+  english: {
+    lover: "let me show you my real heart ",
+    parent: "I'm glad that i'm your child",
+    child: "just be happy and healthy, my sweetheart🙉 ",
+    friend: "I'm always cheering for you my friend",
+  },
+};
--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,9 +1,12 @@
 import React, { ReactElement, useState } from "react";
 import styled from "styled-components";
 import NavBar from "../components/nav";
-import Letter from "../components/letter";
 import contents from "../common/ask";
 import btnIcon from "../assets/btnIcon.svg";
+import lover from "../assets/lover.jpg";
+import parent from "../assets/parent.jpg";
+import child from "../assets/child.jpg";
+import friend from "../assets/friend.jpg";
 
 export default function Write(): ReactElement {
   const [letterState, setLetterState] = useState("lover");
@@ -17,47 +20,110 @@ export default function Write(): ReactElement {
     <Main>
       <NavBar />
       <Container theme={letterState}>
-        <SelectWraper>
-          <SelectState className="state" onChange={stateHandler}>
-            <option value="lover">To 연인</option>
-            <option value="parent">To 부모님</option>
-            <option value="child">To 자녀</option>
-            <option value="friend">To 친구</option>
-          </SelectState>
-        </SelectWraper>
-        {currentLanguage === "korean"
-          ? letterState === "lover"
-            ? contents.korean.lover.map((el: any) => {
-                return (
-                  <ContentChapter>
-                    <ContentTitle>Title : {el.title}</ContentTitle>
-                    {el.question
-                      ? el.question.map((question: any) => {
-                          return (
-                            <ContentQuestion>
-                              {question}
-                              <QuestionInput />
-                            </ContentQuestion>
-                          );
-                        })
-                      : null}
-                    {el.option
-                      ? el.option.map((option: any) => {
-                          return (
-                            <ContentOption>
-                              <Option1>{option[0]}</Option1>
-                              <Option2>{option[1]}</Option2>
-                            </ContentOption>
-                          );
-                        })
-                      : null}
-                  </ContentChapter>
-                );
-              })
-            : ""
-          : null}
+        <ContainerDescription
+          style={
+            letterState === "lover"
+              ? { backgroundImage: `url(${lover})` }
+              : letterState === "parent"
+              ? { backgroundImage: `url(${parent})` }
+              : letterState === "child"
+              ? { backgroundImage: `url(${child})` }
+              : { backgroundImage: `url(${friend})` }
+          }
+        >
+          <SelectWraper>
+            <SelectState className="state" onChange={stateHandler}>
+              {currentLanguage === "korean" ? (
+                <>
+                  {" "}
+                  <option value="lover">To 연인</option>
+                  <option value="parent">To 부모님</option>
+                  <option value="child">To 자녀</option>
+                  <option value="friend">To 친구</option>
+                </>
+              ) : (
+                <>
+                  {" "}
+                  <option value="lover">To Lover</option>
+                  <option value="parent">To Parents</option>
+                  <option value="child">To Child</option>
+                  <option value="friend">To Friend</option>
+                </>
+              )}
+            </SelectState>
+          </SelectWraper>
+        </ContainerDescription>
+        {currentLanguage === "korean" && letterState === "lover" ? <LetterToLover /> : null}
+        {currentLanguage === "korean" && letterState === "parent" ? <LetterToParent /> : null}
       </Container>
     </Main>
+  );
+}
+
+function LetterToLover(): any {
+  return (
+    <>
+      {contents.korean.lover.map((el: any) => {
+        return (
+          <ContentChapter key={el.title}>
+            <ContentTitle>{el.title}</ContentTitle>
+            {el.question
+              ? el.question.map((question: any) => {
+                  return (
+                    <ContentQuestion key={question}>
+                      {question}
+                      <QuestionInput />
+                    </ContentQuestion>
+                  );
+                })
+              : null}
+            {el.option
+              ? el.option.map((option: any) => {
+                  return (
+                    <ContentOption key={option[0]}>
+                      <Option1>{option[0]}</Option1>
+                      <Option2>{option[1]}</Option2>
+                    </ContentOption>
+                  );
+                })
+              : null}
+          </ContentChapter>
+        );
+      })}
+    </>
+  );
+}
+function LetterToParent(): any {
+  return (
+    <>
+      {contents.korean.parent.map((el: any) => {
+        return (
+          <ContentChapter key={el.title}>
+            <ContentTitle>{el.title}</ContentTitle>
+            {el.question
+              ? el.question.map((question: any) => {
+                  return (
+                    <ContentQuestion key={question}>
+                      {question}
+                      <QuestionInput />
+                    </ContentQuestion>
+                  );
+                })
+              : null}
+            {el.option
+              ? el.option.map((option: any) => {
+                  return (
+                    <ContentOption key={option[0]}>
+                      <Option1>{option[0]}</Option1>
+                      <Option2>{option[1]}</Option2>
+                    </ContentOption>
+                  );
+                })
+              : null}
+          </ContentChapter>
+        );
+      })}
+    </>
   );
 }
 
@@ -68,6 +134,12 @@ const Container = styled.div`
   margin: auto;
   margin-top: 3rem;
   min-height: 40rem;
+`;
+const ContainerDescription = styled.div`
+  // background-image: url(${lover});
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 30rem;
 `;
 const SelectState = styled.select`
   width: 10rem;
@@ -87,7 +159,7 @@ const SelectState = styled.select`
 const SelectWraper = styled.div``;
 
 const ContentChapter = styled.div`
-  background-color: #93da9f;
+  background-color: #f7bfdc;
   display: flex;
   flex-direction: column;
   min-height: 15rem;
@@ -99,6 +171,7 @@ const ContentTitle = styled.div`
 `;
 const ContentQuestion = styled.div`
   margin-top: 1rem;
+  font-size: 1.2rem;
   display: flex;
   flex-direction: column;
 `;
